183f92afb16ac269d01de4d6f7e7219aeaa11848
João Pedro Barbosa <joppemdesu@gmail.com>
2023-12-18 15:05:03 -0400

    MusicListAdapter has been created

--- app/src/main/java/com/example/mymusicapp/MusicListAdapter.java
+++ app/src/main/java/com/example/mymusicapp/MusicListAdapter.java
@@ -1,14 +1,53 @@
 package com.example.mymusicapp;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import org.jetbrains.annotations.NotNull;
 
-public class MusicListAdapter {
-    
+import java.util.ArrayList;
+
+public class MusicListAdapter extends RecyclerView.Adapter<MusicListAdapter.ViewHolder>{
+
+    ArrayList<AudioModel> songsList;
+    Context context;
+
+    public MusicListAdapter(ArrayList<AudioModel> songsList, Context context) {
+        this.songsList = songsList;
+        this.context = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.recycler_item, parent,
+                false);
+        return new MusicListAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MusicListAdapter.ViewHolder holder, int position) {
+        AudioModel songData = songsList.get(position);
+        holder.titleTextView.setText(songData.getTitle());
+
+    }
+    @Override
+    public int getItemCount() {
+        return songsList.size();
+    }
+
     public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView titleTextView;
+        ImageView imageViewIcon;
         public ViewHolder(View itemview){
             super(itemview);
+            titleTextView = itemview.findViewById(R.id.music_title_text);
+            imageViewIcon = itemview.findViewById(R.id.icon_view);
         }
     }
 }
